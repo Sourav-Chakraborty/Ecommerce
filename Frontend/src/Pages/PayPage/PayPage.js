15ede25a4378e31f3774656a29351f719e0f99c9
Sourav <chakrabortysourav801@gmail.com>
2022-03-11 12:00:30 +0530

    final commit (may be)

--- Frontend/src/Pages/PayPage/PayPage.js
+++ Frontend/src/Pages/PayPage/PayPage.js
@@ -1,4 +1,4 @@
-import { Button, Container, Grid } from "@material-ui/core";
+import { Box, Button, Container, Grid, Modal, Typography } from "@material-ui/core";
 import axios from "axios";
 import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
@@ -12,12 +12,23 @@ class PayPage extends Component {
     super(props);
     if (!this.props.isLoggedin) this.props.history.push("/login");
     this.state = {
+      open:false,
       totalCost: 0,
       env: "sandbox",
       currency: "USD",
     };
   }
-
+  style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
   loadScript = (src) => {
     return new Promise((resolve) => {
       const script = document.createElement("script");
@@ -80,12 +91,7 @@ class PayPage extends Component {
         );
         console.log("result is", result);
         if (result.data.msg) {
-          this.props.changeAlert(true, "success", "Successfully paid");
-          setTimeout(() => {
-            this.props.changeAlert(false);
-          }, 5000);
-          this.props.history.push("/");
-          this.props.cartItem(0);
+          this.handleModalOpen()
         }
       },
 
@@ -99,7 +105,12 @@ class PayPage extends Component {
     const paymentObject = new window.Razorpay(options);
     paymentObject.open();
   };
-
+  handleModalOpen=()=>{
+    this.setState((prevstate)=>{
+      prevstate.open=true
+      return prevstate
+    })
+  }
   handlePayment = async () => {
     const config = {
       headers: {
@@ -138,12 +149,7 @@ class PayPage extends Component {
       config
     );
     if (response.data.msg) {
-      this.props.changeAlert(true, "success", "Successfully paid");
-      setTimeout(() => {
-        this.props.changeAlert(false);
-      }, 5000);
-      this.props.history.push("/");
-      this.props.cartItem(0);
+      this.handleModalOpen()
     }
   };
   onError = (err) => {
@@ -152,6 +158,18 @@ class PayPage extends Component {
   onCancel = (data) => {
     console.log("The payment was cancelled!", data);
   };
+  handleClose=()=>{
+    this.setState((prevstate)=>{
+      prevstate.open=false
+      return prevstate
+    })
+    this.props.changeAlert(true, "success", "Successfully paid");
+      setTimeout(() => {
+        this.props.changeAlert(false);
+      }, 5000);
+      this.props.history.push("/");
+      this.props.cartItem(0);
+  }
   render() {
     let env = "sandbox";
     let currency = "USD";
@@ -165,12 +183,15 @@ class PayPage extends Component {
       <div>
         <Container style={{ height: "80vh" }}>
           <Grid align="center" className="midOfScreen">
+            
+
+           {parseInt(this.state.totalCost)>0? <>
             <h6>
               Remember for paypal transaction will be through us dollar only
               that is Rs {this.state.totalCost} ={" "}
               {parseInt(this.state.totalCost) / 75} USD
             </h6>
-
+           
             <PaypalExpressBtn
               env={env}
               client={client}
@@ -193,7 +214,27 @@ class PayPage extends Component {
               color="success"
             >
               pay by card/upi/net banking
-            </Button>
+            </Button></>:<>
+              <h2 className="text-center">Your cart is empty,add something to pay </h2>
+            
+            </>
+            }
+          
+            <Modal
+        open={this.state.open}
+        onClose={this.handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={this.style}>
+          <Typography id="modal-modal-title" style={{color:"green"}} variant="h4" component="h1">
+           Success,
+          </Typography>
+          <Typography style={{color:"red"}} id="modal-modal-description" sx={{ mt: 2 }}>
+            You have Successfully paid & placed your order
+          </Typography>
+        </Box>
+      </Modal>
           </Grid>
         </Container>
       </div>
